1a62662262a2463b1fe338b911fdbb4575cf2f55
simonjisu <simonjisu@gmail.com>
2022-07-02 07:55:31 +0900

    update figures

--- app.py
+++ app.py
@@ -108,17 +108,28 @@ def get_exp_name(sm, nsn, nsg, ntrm, fm):
     exp_name = '_'.join(args)
     return exp_name
 
+# @st.cache
+def get_df_gaps(cache_path):
+    # df_gaps
+    df_gaps = pd.read_csv(cache_path / 'all_results.csv', encoding='utf-8')
+    df_gaps['method'] = df_gaps['exp_name'].str.extract('(abs\_interaction|ratio|abs)')
+    df_gaps['exps'] = df_gaps.loc[:, 'exp_name'].values.copy()
+    for method in df_gaps['method'].unique():
+        cond = df_gaps['method'] == method
+        df_gaps.loc[cond, 'exps'] = df_gaps.loc[cond, 'exps'].str.slice(start=len(method))
+    # df_gaps['exp_name'] = df_gaps['exp_name'].str.slice(start=1)
+    rename_cols = {0: 'n_select_scores', 1: 'n_select_gap', 2: 'nodes_to_run_method', 3: 'filter_method'}
+    df_gaps = pd.concat([df_gaps, df_gaps['exps'].str.slice(start=1).str.split('_', expand=True).rename(columns=rename_cols)], axis=1)
+    df_gaps.drop(columns=['exps'], inplace=True)
+    df_gaps['n_select_scores'] = df_gaps['n_select_scores'].astype(np.int32)
+    df_gaps['n_select_gap'] = df_gaps['n_select_gap'].astype(np.int32)
+    return df_gaps
+
 
 dataset_names = ['titanic', 'adult', 'boston', 'california']
 cache_path = Path('.').resolve() / 'cache' 
 cache = load_cache(cache_path, dataset_names)
-# df_gaps
-df_gaps = pd.read_csv(cache_path / 'all_results.csv', encoding='utf-8')
-df_gaps['methods'] = df_gaps['exp_name'].str.extract('(abs\_interaction|ratio|abs)')
-for method in df_gaps['methods'].unique():
-    cond = df_gaps['methods'] == method
-    df_gaps.loc[cond, 'exp_name'] = df_gaps.loc[cond, 'exp_name'].str.slice(start=len(method))
-df_gaps['exp_name'] = df_gaps['exp_name'].str.slice(start=1)
+df_gaps = get_df_gaps(cache_path)
 
 score_method_list = ['abs', 'abs_interaction', 'ratio']
 n_select_scores_list = [5, 10]
@@ -183,23 +194,26 @@ st_shap(shap.plots.bar(shap_values, max_display=20), height=400, width=700)
 st.write(DESC[ds_name])
 
 st.write('# Experiment Plots')
-rename_cols = {0: 'n_select_scores', 1: 'n_select_gap', 2: 'nodes_to_run_method', 3: 'filter_method'}
 
-df_temp = (df_gaps.loc[(df_gaps['dataset'] == ds_name) & (df_gaps['methods'] == score_method), ['exp_name', 'step', 'gaps']]).copy()
-df_exps = df_temp['exp_name'].str.split('_', expand=True).rename(columns=rename_cols)
+op_sm = st.multiselect('Score Method', options=score_method_list)
+op_nss = st.multiselect('Maximum Numer of selecting candidates', options=n_select_scores_list)
+op_nsg = st.multiselect('Maximum Numer of keeping best gap', options=n_select_gap_list)
+op_ntrm = st.multiselect('Way to select nodes', options=nodes_to_run_method_list)
+op_fm = st.multiselect('Way to filter nodes', options=filter_method_list)
 
-op_nss = st.multiselect('Maximum Numer of selecting candidates', options=df_exps['n_select_scores'].unique())
-op_nsg = st.multiselect('Maximum Numer of keeping best gap', options=df_exps['n_select_gap'].unique())
-op_ntrm = st.multiselect('Way to select nodes', options=df_exps['nodes_to_run_method'].unique())
-op_fm = st.multiselect('Way to filter nodes', options=df_exps['filter_method'].unique())
+score_method_list = ['abs', 'abs_interaction', 'ratio']
+n_select_scores_list = [5, 10]
+n_select_gap_list = [5, 10]
+nodes_to_run_method_list = ['random', 'sort', 'full']
+filter_method_list = ['random', 'sort', 'prob']
+
+
+df_exps = df_gaps.loc[(df_gaps['dataset'] == ds_name), :]
+
 if op_nss and op_nsg and op_ntrm and op_fm:
-    cond = df_exps['n_select_scores'].isin(op_nss) & df_exps['n_select_gap'].isin(op_nsg) & \
+    cond = df_exps['method'].isin(op_sm) & df_exps['n_select_scores'].isin(op_nss) & df_exps['n_select_gap'].isin(op_nsg) & \
         df_exps['nodes_to_run_method'].isin(op_ntrm) & df_exps['filter_method'].isin(op_fm)
-    df_draw = df_temp.loc[df_exps.index[cond]]
-    
+    df_draw = df_exps.loc[df_exps.index[cond], ['step', 'exp_name', 'gaps']]
     pivot_table = df_draw.pivot(index='step', columns='exp_name')['gaps']
     st.write(pivot_table.T)
     st.line_chart(pd.DataFrame(pivot_table.values, index=pivot_table.index, columns=pivot_table.columns.values), use_container_width=True)
-
-
-# st.line_chart(df_gaps.set_index(['idx', 'exp_name']).unstack('exp_name')['gaps'])
