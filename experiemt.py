659b6b37e040c40d7a01072287f7a4a153feca68
simonjisu <simonjisu@gmail.com>
2022-07-02 07:25:00 +0900

    update linux version

--- experiemt.py
+++ experiemt.py
@@ -147,4 +147,4 @@ if __name__ == '__main__':
     parser.add_argument('--force_rerun', action='store_true')
     args = parser.parse_args()
     main(infos=args.infos, force_rerun=args.force_rerun)
-    record()
+    # record()
